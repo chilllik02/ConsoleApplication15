238827e4b611b301ae3940952579383e8732e7a6
chilllik02 <andrei02.03.2019@gmail.com>
2024-12-16 14:47:16 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,43 @@
+﻿#include <locale.h>
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    string password;
+    getline(cin, password);
+    
+    int count = 0;
+    bool isvalid = true;
+
+    if (!(password.size() >= 8 && password.size() < 14))
+    {
+        isvalid = false;
+    }
+
+    for (char i : password)
+    {
+        
+        if (isupper(i) || isdigit(i))
+        {
+            count++;
+
+        }
+
+
+    }
+
+    if (count < 3)
+    {
+        isvalid = false;
+        
+    }
+    
+    string output = (isvalid) ? "YES" : "NO";
+
+    cout << output;
+
+}
+
